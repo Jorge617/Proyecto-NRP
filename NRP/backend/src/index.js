8036a128b44eef895e9856967eeb13cee0ee76df
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-10-18 17:21:58 +0200

    Correccion calculo de satisfaccion

--- NRP/backend/src/index.js
+++ NRP/backend/src/index.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 require('./database');
+const npr = require('./Npr')
 async function main() {
     await app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
+    
+    
+    npr.calcularPrioridad();
 }
 
 main();
--- NRP/backend/src/npr.js
+++ NRP/backend/src/npr.js
@@ -1,29 +1,41 @@
-const axios = require('axios');
-
-clientes= [];
-requisitos= [];
 
 
-async function main(){
+
+
+async function calcularPrioridad() {
+
+    const axios = require('axios');
+
+    clientes = [];
+    requisitos = [];
+    ordenPrioridad = [];
+
     const resCliente = await axios.get('http://localhost:4000/clientes/')
-    clientes= resCliente.data
+    clientes = resCliente.data
 
     const resRequisito = await axios.get('http://localhost:4000/requisitos/')
-    requisitos= resRequisito.data
+    requisitos = resRequisito.data
 
 
-    var suma = 0;
+    var suma;
 
-   for(var i = 0; i < requisitos.length; i++){
-       for(var j = 0; j < requisitos[i].prioridad.length; j++){
-       suma += clientes.find(element => element._id == requisitos[i].prioridad[j].cliente).importancia * requisitos[i].prioridad[j].valor
-        
+    for (var i = 0; i < requisitos.length; i++) {
+        suma = 0;
+
+        for (var j = 0; j < requisitos[i].prioridad.length; j++) {
+            suma += clientes.find(element => element._id == requisitos[i].prioridad[j].cliente).importancia * requisitos[i].prioridad[j].valor
         }
+        ordenPrioridad.push(suma);        
+
+        ordenPrioridad.sort(function(a, b){return b-a});
+
     }
 
-    console.log(suma);
+    return ordenPrioridad;
+}
+
+module.exports = {
+    'calcularPrioridad': calcularPrioridad
 }
 
 
-
-main();
