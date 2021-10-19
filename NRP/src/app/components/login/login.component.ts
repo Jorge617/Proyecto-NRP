aba30a7c4c5ee5f50a2741345d6f7f592a180002
guillermo montalban martinez <guillermosysti@gmail.com>
2021-10-19 11:34:36 +0200

    acceso a los campos del backend

--- NRP/src/app/components/login/login.component.ts
+++ NRP/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public nombre:string;
+  public password:string;
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    this.nombre = "";
+    this.password = "";
+  }
 
   ngOnInit(): void {
+    // this.login("nombre","nombre");
+  }
+
+  validateLogin() {
+    /*
+    var campoNombre = $("form2Example11");
+    var campoPassword = $("form2Example22");
+    var botonLogin = $("botonLogin");
+
+    this.nombre = campoNombre.val();
+    this.password = campoPassword.val();
+    botonLogin.click(function(){
+      */
+    botonLogin.(click)="this.http.get("http/localhost/4000/Usuarios")";
+    var respond = this.http.get("http/localhost/4000/Usuarios");
+    respond.
   }
 
 }
