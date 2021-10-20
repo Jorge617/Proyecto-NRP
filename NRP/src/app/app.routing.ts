e3e059870f9cd7374ac72affe2c77282d62725b7
guillermo montalban martinez <guillermosysti@gmail.com>
2021-10-20 17:13:34 +0200

    creación del componente inicio y barra de navegacion dentro del componente

--- NRP/src/app/app.routing.ts
+++ NRP/src/app/app.routing.ts
@@ -1,14 +1,15 @@
-import { ModuleWithProviders } from "@angular/core";
+import { ModuleWithProviders, Component } from '@angular/core';
 import { Routes,RouterModule, Route } from "@angular/router";
 
 import { LoginComponent } from "./components/login/login.component";
 import { RegistroComponent } from './components/registro/registro.component';
-
+import { InicioComponent } from "./components/inicio/inicio.component";
 
 
 const appRoutes : Routes = [
     {path : '', component : LoginComponent},
-    {path : 'registro', component : RegistroComponent}, 
+    {path : 'registro', component : RegistroComponent},
+    {path : 'inicio', component : InicioComponent},
     {path : '**', component : LoginComponent}
 ];
 
