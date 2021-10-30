81ed3fbfb0fc9508ded5967532a15461beb879e1
guillermo montalban martinez <guillermosysti@gmail.com>
2021-10-31 00:14:28 +0200

    creación del componente proyecto-cliente y arreglar login para el cliente

--- NRP/src/app/components/inicio-cliente/inicio-cliente.component.ts
+++ NRP/src/app/components/inicio-cliente/inicio-cliente.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-inicio-cliente',
   templateUrl: './inicio-cliente.component.html',
-  styleUrls: ['./inicio-cliente.component.css']
+  styleUrls: ['./inicio-cliente.component.css'],
+  providers: [UsuarioService]
 })
 export class InicioClienteComponent implements OnInit {
 
-  constructor() { }
-
+  public usuario:Usuario = new Usuario("","","","","",0,false,[],"");
+  constructor(private _usuarioService:UsuarioService,public router: Router) { 
+    
+  }
+  
   ngOnInit(): void {
+   this.getUserLogged();
+  }
+
+  getUserLogged(){
+    this._usuarioService.getUserLogged(this.usuario);
+   
+  }
+
+  cerrarSesion(){
+    this._usuarioService.deleteTokenCookies();
+    this.router.navigateByUrl("/login");
   }
 
 }
