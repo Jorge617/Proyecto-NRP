8295bf3f5e0432d51416aa661bc6ba3c5da8084b
JORGE MORA CAMPOY <jmc617@inlumine.ual.es>
2021-10-30 02:59:37 +0200

    Maquetación vista proyecto

--- NRP/src/app/services/proyecto.service.ts
+++ NRP/src/app/services/proyecto.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Global } from './global';
 import { Usuario } from '../models/usuario';
@@ -10,24 +10,33 @@ import { Proyecto } from '../models/proyecto';
 
 
 @Injectable()
-export class ProyectoService{
-    public url:string;
+export class ProyectoService {
+    public url: string;
 
-    constructor(private _http: HttpClient,private cookies: CookieService){
+    constructor(private _http: HttpClient, private cookies: CookieService) {
         this.url = Global.url;
-        
+
     }
 
-    getProyecto(id:any) : Observable<any>{
+    getProyectos() {
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
-		return this._http.get(this.url+'projectos/'+id, {headers: headers});
+        return this._http.get(this.url + 'projectos', { headers: headers });
     }
 
-    crearProyecto(proyecto:Proyecto):Observable<any>{
+    getProyecto(id: any): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.get(this.url + 'projectos/' + id, { headers: headers });
+    }
+
+    crearProyecto(proyecto: Proyecto): Observable<any> {
         let params = JSON.stringify(proyecto);
-        console.log(params)
-        let headers =  new HttpHeaders().set('Content-Type','application/json');
-        return this._http.post(this.url + "proyectos",params,{headers:headers});
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.post(this.url + "proyectos", params, { headers: headers });
+    }
+
+    deleteProyecto(proyecto: Proyecto): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.delete(this.url + "proyectos/" + proyecto.id, { headers: headers });
     }
 }
 
